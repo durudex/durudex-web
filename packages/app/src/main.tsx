a90b372ebe37ff97aefd7483c9fc414b9d346f7b
chaurka <chaurka.noyan@gmail.com>
2022-07-05 19:44:12 +0300

    Change license notice once again

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,9 +1,20 @@
-/**
- * Copyright (C) 2022 Durudex
+/*
+ * Copyright © 2022 Durudex
  *
- * This source code is licensed under the GNU Affero General Public License
- * found in the COPYING file in the root directory of this source tree.
+ * This file is part of Durudex: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Durudex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Durudex. If not, see <https://www.gnu.org/licenses/>.
  */
+
 import {render} from 'solid-js/web'
 
 render(() => <>Hello, world!</>, document.body)
--- packages/test-api/test-api.js
+++ packages/test-api/test-api.js
@@ -1,8 +1,18 @@
-/**
- * Copyright (C) 2022 Durudex
+/*
+ * Copyright © 2022 Durudex
  *
- * This source code is licensed under the GNU Affero General Public License
- * found in the COPYING file in the root directory of this source tree.
+ * This file is part of Durudex: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Durudex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Durudex. If not, see <https://www.gnu.org/licenses/>.
  */
 
 import {createServer} from 'node:http'
@@ -10,8 +20,12 @@ import cc from 'concolor'
 
 const API_URL = 'https://api.dev.durudex.com/query'
 
+/**
+ * Start a http server that logs requests to the console and responds with the data development API returns.
+ * @returns Close server function
+ */
 export function startApi(port) {
-  const {close} = createServer((req, res) => {
+  const server = createServer((req, res) => {
     console.log(cc.info`\n[api request]\n`)
 
     req.on('data', chunk => {
@@ -35,6 +49,8 @@ export function startApi(port) {
         })
     })
   }).listen(port)
+
+  const close = server.close.bind(server)
 
   process.on('beforeExit', close)
 
