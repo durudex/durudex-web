e693ae1387ef71b16a5c4e81a1baf06f6d4106d0
chaurka <chaurka.noyan@gmail.com>
2022-07-05 12:12:09 +0300

    Add copyright notes, add api proxy server

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2022 Durudex
+ *
+ * This source code is licensed under the GNU Affero General Public License
+ * license found in the LICENSE file in the root directory of this source tree.
+ */
+
 import {render} from 'solid-js/web'
 
 render(() => <>Hello, world!</>, document.body)
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,7 +1,33 @@
+/**
+ * Copyright (C) 2022 Durudex
+ *
+ * This source code is licensed under the GNU Affero General Public License
+ * license found in the LICENSE file in the root directory of this source tree.
+ */
+
 import {defineConfig} from 'vite'
 import solid from 'vite-plugin-solid'
 import checker from 'vite-plugin-checker'
 import {resolve} from 'node:path'
+import {createServer} from 'node:http'
+
+if (process.argv[2] === 'build') {
+  createServer((req, res) => {
+    req.on('data', chunk => {
+      fetch('https://api.dev.durudex.com/query', {
+        body: chunk,
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+      })
+        .then(apiResp => apiResp.json())
+        .then(content => {
+          console.log('[api request]\n')
+          console.dir(content)
+          res.end(JSON.stringify(content))
+        })
+    })
+  }).listen(3001)
+}
 
 const relative = (dir: string) => resolve(process.cwd(), dir)
 
@@ -11,9 +37,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/dev-api': {
-        target: 'http://localhost:3001'
-      }
-    }
+        target: 'http://localhost:3001',
+      },
+    },
   },
-  resolve: {alias: {$: relative('src/')}}
+  resolve: {alias: {$: relative('src/')}},
 })
